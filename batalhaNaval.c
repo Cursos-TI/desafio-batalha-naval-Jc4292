cd54d0585724c402302d4eda08ae3cdf4f50e5b2
Jc4292 <jcaesar7637@gmail.com>
2025-04-28 18:31:07 +0000

    ships and grid implemented

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,7 +9,31 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    const int HEIGHT=5, WIDTH=5;
+    int matriz_2d[WIDTH][HEIGHT];
+    char linha[10] = {'A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K'};
 
+    int navio_1[3] = {4, 1, 3};
+    int navio_2[3] = {0, 0, 2};
+    
+    printf("  ");
+    for(int x = 0; x<WIDTH; x++){
+        printf("%c", linha[x]);
+    }
+    printf("\n");
+    for(int y = 0; y<HEIGHT; y++){
+        printf("%d ", y+1);
+        for(int x = 0; x<WIDTH; x++){
+            if(y==navio_1[0]&&x>=navio_1[1]&&x<=navio_1[2])
+                matriz_2d[x][y] = 3;
+            else if(x==navio_2[0]&&y>=navio_2[1]&&y<=navio_2[2])
+                matriz_2d[x][y] = 3;
+            else
+                matriz_2d[x][y] = 0;
+            printf("%d", matriz_2d[x][y]);
+        }
+        printf("\n");
+    }
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
