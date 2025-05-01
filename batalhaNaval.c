a9d7ca9ba9b9e6a3e7ffde1ac79747f34e62d904
Jc4292 <jcaesar7637@gmail.com>
2025-05-01 16:55:02 +0000

    special powers

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -20,17 +20,37 @@ int main() {
                         - index 1: posição incial (tamanho),
                         - index 2: posição final (tamanho),
     */
-    int navio_1[3] = {4, 1, 3};
+    //int navio_1[3] = {4, 1, 3};
 
-    int navio_3[3] = {6, 2, 4};
+    //int navio_3[3] = {6, 2, 4};
 
     /* navio vertical   - index 0: sua posição horizontal,
                         - index 1: posição incial (tamanho),
                         - index 2: posição final (tamanho),
     */
-    int navio_2[3] = {2, 0, 2};
+    //int navio_2[3] = {2, 0, 2};
 
-    int navio_4[3] = {8, 4, 6};
+    //int navio_4[3] = {8, 4, 6};
+
+    int cone_pos[2] = {1, 0};
+    int cone[3][5]={{0,0,1,0,0},
+                    {0,1,1,1,0},
+                    {1,1,1,1,1}};
+    
+    int cruz_pos[2] = {4, 4};
+    int cruz[5][5]={{0,0,2,0,0},
+                    {0,0,2,0,0},
+                    {2,2,2,2,2},
+                    {0,0,2,0,0},
+                    {0,0,2,0,0}};
+    
+    int oct_pos[2] = {0, 3};
+    int octaedro[5][5]={{0,0,0,0,0},
+                        {0,0,3,0,0},
+                        {0,3,3,3,0},
+                        {0,0,3,0,0},
+                        {0,0,0,0,0}};
+
 
 
     // bloco de código que exibe e inicializa todos os valores da grid,
@@ -45,45 +65,31 @@ int main() {
 
             // hardcode de posicionamento dos navios
             
-            if(y==navio_1[0]&&x>=navio_1[1]&&x<=navio_1[2])
-                matriz_2d[x][y] = 3;
-            else if(x==navio_2[0]&&y>=navio_2[1]&&y<=navio_2[2])
-                matriz_2d[x][y] = 3;
-            else if(y==navio_3[0]&&x>=navio_3[1]&&x<=navio_3[2])
-                matriz_2d[x][y] = 3;
-            else if(x==navio_4[0]&&y>=navio_4[1]&&y<=navio_4[2])
-                matriz_2d[x][y] = 3;
-            else
+            // if(y==navio_1[0]&&x>=navio_1[1]&&x<=navio_1[2])
+            //     matriz_2d[x][y] = 3;
+            // else if(x==navio_2[0]&&y>=navio_2[1]&&y<=navio_2[2])
+            //     matriz_2d[x][y] = 3;
+            // else if(y==navio_3[0]&&x>=navio_3[1]&&x<=navio_3[2])
+            //     matriz_2d[x][y] = 3;
+            // else if(x==navio_4[0]&&y>=navio_4[1]&&y<=navio_4[2])
+            //     matriz_2d[x][y] = 3;
+            // else
+            if((x>=cone_pos[0]&&x-cone_pos[0]<5)&&(y>=cone_pos[1]&&y-cone_pos[1]<3)) {
+                matriz_2d[x][y] = cone[y-cone_pos[1]][x-cone_pos[0]];
+            }
+            else if((x>=cruz_pos[0]&&x-cruz_pos[0]<5)&&(y>=cruz_pos[1]&&y-cruz_pos[1]<5)) {
+                matriz_2d[x][y] = cruz[y-cruz_pos[1]][x-cruz_pos[0]];
+            }
+            else if((x>=oct_pos[0]&&x-oct_pos[0]<5)&&(y>=oct_pos[1]&&y-oct_pos[1]<5)) {
+                matriz_2d[x][y] = octaedro[y-oct_pos[1]][x-oct_pos[0]];
+            }
+            else {
                 matriz_2d[x][y] = 0;
+            }
             printf("%d", matriz_2d[x][y]);
         }
         printf("\n");
     }
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
